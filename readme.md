8aba60484223005d892043ddc78e871c205d08ad
github-ascnet <github.office@ascnet.ch>
2022-06-02 08:51:27 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Tailwind Mobile Flex Reverse
+
+A Tailwind  approach to switch flex items in the reverse order on smaller devices
+
+## Usage
+
+Install dependencies
+
+```
+npm install
+```
+
+Run Tailwind CLI
+
+```
+npm run watch
+```
+
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        softBlue: "hsl(231, 69%, 60%)",
+      },
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
